33ec07a58c96d7ef64f05c8dc63e64d4d184a701
Al Viro <viro@zeniv.linux.org.uk>
2013-06-29 13:23:08 +0400

    ext4: ->tmpfile() support
    very similar to ext3 counterpart...
    
    Change-Id: Ibb9de458c172ad50c4c202b971cb7243c8e43c82
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -2501,11 +2501,11 @@ retry:
 		inode->i_op = &ext4_file_inode_operations;
 		inode->i_fop = &ext4_file_operations;
 		ext4_set_aops(inode);
-		d_tmpfile(dentry, inode);
 		err = ext4_orphan_add(handle, inode);
 		if (err)
-			goto err_unlock_inode;
+			goto err_drop_inode;
 		mark_inode_dirty(inode);
+		d_tmpfile(dentry, inode);
 		unlock_new_inode(inode);
 	}
 	if (handle)
@@ -2513,9 +2513,10 @@ retry:
 	if (err == -ENOSPC && ext4_should_retry_alloc(dir->i_sb, &retries))
 		goto retry;
 	return err;
-err_unlock_inode:
+err_drop_inode:
 	ext4_journal_stop(handle);
 	unlock_new_inode(inode);
+	iput(inode);
 	return err;
 }
 
