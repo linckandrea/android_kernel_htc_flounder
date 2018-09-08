65ff35a09d87ad18a585a4e3dfdb77e61005da7b
mortezadadgar <mortezadadgar97@gmail.com>
2018-09-08 15:11:23 +0000

    treewide: Fix -Wmaybe-uninitialized warnings
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- kernel/posix-cpu-timers.c
+++ kernel/posix-cpu-timers.c
@@ -1393,7 +1393,7 @@ void set_process_cpu_timer(struct task_struct *tsk, unsigned int clock_idx,
 	if (cpu_timer_sample_group(clock_idx, tsk, &now))
 		return;
 
-	if (oldval) {
+	if (!ret && oldval) {
 		/*
 		 * We are setting itimer. The *oldval is absolute and we update
 		 * it to be relative, *newval argument is relative and we update
