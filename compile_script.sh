8beaf18174a0e95b061605447c3a9686a8a1592e
Andrea <linckandrea01@gmail.com>
2021-06-10 22:05:06 +0200

    import anykernel3

--- compile_script.sh
+++ compile_script.sh
@@ -3,6 +3,13 @@ export ARCH=arm64
 export SUBARCH=arm64
 make O=out clean
 make O=out mrproper
-export CROSS_COMPILE=/home/andrea/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/andrea/android/toolchains-64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 make O=out lineage_flounder_defconfig
 make O=out -j$(nproc --all)
+
+echo
+echo "Making anykernel zip"
+echo
+cp ./out/arch/arm64/boot/Image.gz-dtb ./AnyKernel3/
+cd ./AnyKernel3
+zip -r9 Pop_kernel-flounder-O-rx-x.zip * -x .git README.md *placeholder
