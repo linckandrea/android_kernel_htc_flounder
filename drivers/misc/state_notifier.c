12137beae50cbe902234c9cf2e220816630ef97d
Andrea <linckandrea01@gmail.com>
2021-06-10 22:42:47 +0200

    state_notifier: enable by default

--- drivers/misc/state_notifier.c
+++ drivers/misc/state_notifier.c
@@ -20,8 +20,8 @@
 /*
  * debug = 1 will print all
  */
-static unsigned int debug;
-module_param_named(debug_mask, debug, uint, 0644);
+static unsigned int debug = 0;
+module_param_named(debug_mask, debug, uint, 0444);
 
 #define dprintk(msg...)		\
 do {				\
@@ -29,8 +29,6 @@ do {				\
 		pr_info(msg);	\
 } while (0)
 
-static bool enabled = true;
-module_param_named(enabled, enabled, bool, 0664);
 static unsigned int suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME;
 module_param_named(suspend_defer_time, suspend_defer_time, uint, 0664);
 static struct delayed_work suspend_work;
@@ -92,7 +90,7 @@ static void _resume_work(struct work_struct *work)
 void state_suspend(void)
 {
 	dprintk("%s: suspend called.\n", STATE_NOTIFIER);
-	if (state_suspended || suspend_in_progress || !enabled)
+	if (state_suspended || suspend_in_progress)
 		return;
 
 	suspend_in_progress = true;
