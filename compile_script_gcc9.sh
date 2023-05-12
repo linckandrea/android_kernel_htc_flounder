7f6ed1df5074cac463590a25a5f3532670e12a3b
Andrea <linckandrea01@gmail.com>
2023-05-12 20:32:40 +0200

    Rebrand to Armonia Kernel

--- compile_script_gcc9.sh
+++ compile_script_gcc9.sh
@@ -1,9 +1,14 @@
+branch=$(git symbolic-ref --short HEAD)
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+last_commit=$(git rev-parse --verify --short=8 HEAD)
+export LOCALVERSION="-Armonia-Kernel-${branch_name}/${last_commit}"
+
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
 make O=out clean
 make O=out mrproper
-export CROSS_COMPILE=/home/andrea/android/toolchains-64/proton-gcc9/bin/aarch64-elf-
+export CROSS_COMPILE="$HOME"/android/toolchains/proton-gcc9/bin/aarch64-elf-
 make O=out lineage_flounder_defconfig
 make O=out -j$(nproc --all)
 
@@ -12,4 +17,5 @@ echo "Making anykernel zip"
 echo
 cp ./out/arch/arm64/boot/Image.gz-dtb ./AnyKernel3/
 cd ./AnyKernel3
-zip -r9 Pop_kernel-flounder-O-rx-x.zip * -x .git README.md *placeholder
+rm *.zip
+zip -r9 Armonia_kernel-flounder-O-${branch_name}-${last_commit}.zip * -x .git README.md *placeholder
